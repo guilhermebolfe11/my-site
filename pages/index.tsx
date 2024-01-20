d30487fc910de9f67c8f617a43c116b71275fdd6
Guilherme Bolfe <bolfeguilherme@gmail.com>
2024-01-20 17:05:36 -0300

    feat: remove sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
-import * as React from 'react';
 import type { NextPage } from 'next';
-import { About } from '../src/components/About/About';
-import { Graduate } from '../src/components/Graduate/Graduate';
-import { Jobs } from '../src/components/Jobs/Jobs';
 import { Stacks } from '../src/components/Stacks/Stacks';
-import { Articles } from '../src/components/Articles/Articles';
 
 const Home: NextPage = () => {
   return (
     <>
-      <About />
-      <Graduate />
-      <Jobs />
       <Stacks />
-      <Articles />
     </>
   );
 };
