ed64e469b1d583355fe2a27482565f7eded7b82f
Guilherme Bolfe <bolfeguilherme@gmail.com>
2024-05-30 17:43:50 -0300

    feat: update footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://www.linkedin.com/in/guilherme-bolfe/"
+            href="https://www.linkedin.com/in/guilherme-bolfe/?locale=en_US"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">LinkedIn</p>
@@ -54,9 +54,6 @@ export default function Footer() {
           </a>
         </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
     </footer>
   )
 }
